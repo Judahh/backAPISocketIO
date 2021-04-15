08788f3ea684dde7223b77498e48689696724bbf
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2021-04-15 15:16:14 -0300

    init

---
+++ test/integration/testController.ts
@@ -0,0 +1,7 @@
+import { BaseController } from 'backapirest';
+
+export default class TestController extends BaseController {
+  protected element = 'test';
+
+  protected elements = 'tests';
+}
---
+++ test/integration/testService.ts
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// file deepcode ignore no-any: any needed
+// file deepcode ignore object-literal-shorthand: argh
+import { BasicService } from 'backapirest';
+export default class TestService extends BasicService {}
